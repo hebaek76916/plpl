2a9e1855ee36deb8409dcf8e77a5714b7a8b9999
baekhyeoneun <hebaek@kocowa.com>
2023-08-22 15:35:03 +0900

    Initial Commit

---
+++ temp1/temp1.h
@@ -0,0 +1,18 @@
+//
+//  temp1.h
+//  temp1
+//
+//  Created by 현은백 on 2023/08/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for temp1.
+FOUNDATION_EXPORT double temp1VersionNumber;
+
+//! Project version string for temp1.
+FOUNDATION_EXPORT const unsigned char temp1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <temp1/PublicHeader.h>
+
+
