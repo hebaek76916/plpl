df90891575ca1cffeffcd9b151eaf664960d9bd0
baekhyeoneun <hebaek@kocowa.com>
2023-09-01 11:00:45 +0900

    Fruit class to objctify!

--- temp1/Fruit.swift
+++ temp1/Fruit.swift
@@ -7,19 +7,36 @@
 
 import Foundation
 
-@objc public class Fruit: NSObject {
+@objc public class Product: NSObject {
+    @objc public let price: CGFloat
+    @objc public let sweet: CGFloat
+    @objc public let name: String
     
-    public struct Product {
-        let price: CGFloat
-        let sweet: CGFloat
-        let name: String
+    public override init() {
+        self.price = 0.0
+        self.sweet = 0.0
+        self.name = "FRUIT"
     }
     
-    public func printProduct(product: Product) {
+    @objc public init(price: CGFloat, sweet: CGFloat, name: String) {
+        self.price = price
+        self.sweet = sweet
+        self.name = name
+        super.init()
+    }
+}
+
+@objc public class Fruit: NSObject {
+    
+    public override init() {}
+    
+    @objc public func printProduct(product: Product) {
         print(product.price)
         print(product.sweet)
         print(product.name)
-        
-        
+        NSLog("\(product.price)")
+        NSLog("\(product.sweet)")
+        NSLog("\(product.name)")
+        dump(product)
     }
 }
